758a933a64b585aa0234ded2ae8a3e02b736e211
Ishamahajan23 <108974410+Ishamahajan23@users.noreply.github.com>
2024-05-28 14:13:20 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+
+
+let btn=document.querySelector("#send");
+btn.addEventListener("click",()=>{
+   savetoDb();
+});
+function savetoDb(){
+    let email=document.querySelector("#email");
+    let mob=document.querySelector("#number");
+    let description=document.querySelector("#des");
+    let data={
+       email:email.value,
+       mobile:mob.value,
+       description:description.value
+    }
+    console.log(data);
+
+}
+
