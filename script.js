f1b0e2a208838fc2979cd9f20ba199227d91e491
isha <ishamahajan933@gmail.com>
2024-06-20 16:21:43 +0530

    update files

--- script.js
+++ script.js
@@ -1,19 +1,21 @@
 
 
-let btn=document.querySelector("#send");
-btn.addEventListener("click",()=>{
-   savetoDb();
-});
-function savetoDb(){
-    let email=document.querySelector("#email");
-    let mob=document.querySelector("#number");
-    let description=document.querySelector("#des");
-    let data={
-       email:email.value,
-       mobile:mob.value,
-       description:description.value
-    }
-    console.log(data);
 
-}
+// let btn=document.querySelector("#send");
+// btn.addEventListener("click",()=>{
+//    savetoDb();
+// });
+// function savetoDb(){
+//     let email=document.querySelector("#email");
+//     let mob=document.querySelector("#number");
+//     let description=document.querySelector("#des");
+//     let data={
+//        email:email.value,
+//        mobile:mob.value,
+//        description:description.value
+//     }
+//     console.log(data);
+
+// }
+
 
